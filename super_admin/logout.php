146090b039302aa55cf103dfcc9ed044e377cb44
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-07-11 03:11:27 +0530

    super admin logout

--- super_admin/logout.php
+++ super_admin/logout.php
@@ -9,6 +9,6 @@ session_unset();
 session_destroy();
 
 echo "<script>
-window.location = 'https://ongoing.website/websites/nimesh-lms-new/super_admin/';
+window.location = 'https://ongoing.website/websites/nimesh-lms-new/';
 </script>
 ";
