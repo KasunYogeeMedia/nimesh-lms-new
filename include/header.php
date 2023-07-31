bf4a4d8973f201bc623e480f6e819170037f3c11
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-07-31 17:51:15 +0530

    user_direct.php add

--- include/header.php
+++ include/header.php
@@ -1,48 +1,51 @@
 <!-- navbar start -->
-    <div class="navbar-area">
-        <!-- navbar top start -->
-        <nav class="navbar navbar-area-1 navbar-area navbar-expand-lg">
-            <div class="container nav-container">
-                <div class="responsive-mobile-menu">
-                    <button class="menu toggle-btn d-block d-lg-none" data-target="#edumint_main_menu" 
-                    aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="icon-left"></span>
-                        <span class="icon-right"></span>
-                    </button>
-                </div>
-                <div class="logo">
-                    <a href="index.php"><img src="assets/img/logo-2.png" alt="img"></a>
-                </div>
-                <div class="nav-left-part nav-right-part-desktop">
-     
-                </div>
-                
-                <div class="collapse navbar-collapse" id="edumint_main_menu">
-                    <ul class="navbar-nav menu-open">
+<div class="navbar-area">
+    <!-- navbar top start -->
+    <nav class="navbar navbar-area-1 navbar-area navbar-expand-lg">
+        <div class="container nav-container">
+            <div class="responsive-mobile-menu">
+                <button class="menu toggle-btn d-block d-lg-none" data-target="#edumint_main_menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="icon-left"></span>
+                    <span class="icon-right"></span>
+                </button>
+            </div>
+            <div class="logo">
+                <a href="index.php"><img src="assets/img/logo-2.png" alt="img"></a>
+            </div>
+            <div class="nav-left-part nav-right-part-desktop">
+
+            </div>
+
+            <div class="collapse navbar-collapse" id="edumint_main_menu">
+                <ul class="navbar-nav menu-open">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="course.php">Courses</a>
+                    </li>
+                    <li>
+                        <a href="about.php">About Us</a>
+                    </li>
+                    <li>
+                        <a href="gallery.php">Gallery</a>
+                    </li>
+                    <li>
+                        <a href="workshop.php">Workshop</a>
+                    </li>
+
+                    <ul class="mobile-btn float-right">
                         <li>
-                            <a href="index.php">Home</a>
+                            <a class="loginlink" href="user_direct.php" >Login</a>
                         </li>
                         <li>
-                            <a href="course.php">Courses</a>
-                        </li>
-                        <li>
-                            <a href="about.php">About Us</a>
-                        </li>
-                        <li>
-                            <a href="gallery.php">Gallery</a>
-                        </li>
-                        <li>
-                            <a href="user_direct.php">Login</a>
-                        </li>
-                        <li>
-                            <a href="register.php">Register</a>
-                        </li>
-                        <li>
-                            <a href="workshop.php">Workshop</a>
+                            <a class="registerlink" href="register.php" style="padding: 7px 30px; background: var(--main-color); border-radius:25px;">Register</a>
                         </li>
                     </ul>
-                </div>
+                </ul>
             </div>
-        </nav>
-    </div>
-    <!-- navbar end -->
+        </div>
+    </nav>
+</div>
+
+<!-- navbar end -->
