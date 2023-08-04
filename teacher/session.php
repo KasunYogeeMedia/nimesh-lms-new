f19c8c47761c6e2afe04f6133a249e3d2ff955af
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-08-04 18:09:01 +0530

    update

--- teacher/session.php
+++ teacher/session.php
@@ -2,10 +2,7 @@
 
 
 
-if (!isset($_SESSION['tid']) || trim($_SESSION['tid']) === '') {
-    header('Location: index.php');
-    exit();
-}
+
 
 $session_id = $_SESSION['tid'];
 /* $session_id = $_SESSION['username']; */
