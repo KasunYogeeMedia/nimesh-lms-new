2c53d3f462087bfa89d1bd7c15443f9f3e7b6811
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-08-04 16:27:44 +0530

    update

--- index.php
+++ index.php
@@ -260,8 +260,11 @@ require_once 'super_admin/dbconfig4.php';
                 <?php
                 // Assuming you have already established a database connection
 
+                $currentMonthEndDate = date("Y-m-t");
+
+
                 // The SQL query
-                $list_query = "SELECT * FROM lmsclass_schlmsle WHERE classtype = 'Free Class' AND add_date2 <= '2023-08-31' AND classstatus =0";
+                $list_query = "SELECT * FROM lmsclass_schlmsle WHERE classtype = 'Free Class' AND add_date2 <= $currentMonthEndDate AND classstatus =1";
 
                 // Execute the query
                 $list_result = mysqli_query($conn, $list_query);
