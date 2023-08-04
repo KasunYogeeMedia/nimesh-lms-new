2a4017661e9972e4da276dd71bbdbf13d9663d46
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-08-04 18:12:21 +0530

    update

--- teacher/home.php
+++ teacher/home.php
@@ -1,16 +1,16 @@
 	<?php
 
 
-
+if (!isset($_SESSION)) {
+	session_start();
+}
 require_once 'includes.php';
 
 require_once("../super_admin/conn.php");
 
 require_once '../super_admin/dbconfig4.php';
 
-if (!isset($_SESSION)) {
-	session_start();
-}
+
 
 if (isset($_SESSION['tid'])) {
 
--- teacher/includes.php
+++ teacher/includes.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'session.php';
+//include 'session.php';
 
 //$username = $_SESSION['username'];
 
