ab4339f96d5f4bae9dc164daf6735cd6cf4fe40c
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-08-19 18:49:27 +0530

    payment

--- admin/bank_payaments.php
+++ admin/bank_payaments.php
@@ -187,13 +187,16 @@ if (isset($_GET['id'])) {
                                                             </td>
                                                             <td>
                                                                 <span>Pay Rs.<?php echo number_format($payment_resalt['amount'], 2); ?></span>
+                                                                 <br>
                                                                 <input type="text" class="classFeeInput" data-payment-id="<?php echo $payment_resalt['pid']; ?>">
+                                                                <br>
                                                                 <button class="btn btn-primary submitClassFee" data-payment-id="<?php echo $payment_resalt['pid']; ?>">Update</button>
                                                             </td>
                                                             <td><?php echo $payment_resalt['pay_type']; ?></td>
                                                             <td><?php echo date_format(date_create($payment_resalt['created_at']), "M d, Y - h:i:s A"); ?></td>
                                                             <td>
                                                                 <input type="date" class="nextPayDateInput" data-payment-id="<?php echo $payment_resalt['pid']; ?>">
+                                                                 <br>
                                                                 <button class="btn btn-primary submitNextPayDate" data-payment-id="<?php echo $payment_resalt['pid']; ?>">Update</button>
                                                             <td>
                                                             <td><?php echo $payment_resalt['coupen'] ?></td>
