d9dcc72d3be14241f69ddbc67e0b2ddd3115acdc
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-08-20 15:20:55 +0530

    payments

--- profile/pay_alert.php
+++ profile/pay_alert.php
@@ -1,4 +1,13 @@
+<?php
+include '../super_admin/conn.php';
+
+
+if($next_due == 1){ ?>
+
 <div class="alert alert-danger alert-dismissible fade show" role="alert">
   <strong>Hi!</strong> Please make payment <a class="text-dark" href="student_profile.php#pay_sec">here.</a>
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
+
+<?php } ?>
+
