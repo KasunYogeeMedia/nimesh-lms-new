75b2092f759d8cbe7c53ed6dab247b45491a2465
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-08-20 15:25:46 +0530

    payments

--- profile/pay_alert.php
+++ profile/pay_alert.php
@@ -2,7 +2,7 @@
 include '../super_admin/conn.php';
 
 
-if($next_due == 1){ ?>
+if($next_due == 0){ ?>
 
 <div class="alert alert-danger alert-dismissible fade show" role="alert">
   <strong>Hi!</strong> Please make payment <a class="text-dark" href="student_profile.php#pay_sec">here.</a>
