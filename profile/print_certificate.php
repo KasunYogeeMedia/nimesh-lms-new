82f68d4fa027c19142bd7dee1840218ac1e7fa2e
Kasun24 <kasun961224@gmail.com>
2023-09-07 16:56:25 +0530

    print page

---
+++ profile/print_certificate.php
@@ -0,0 +1,192 @@
+<html>
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Crimson+Text&family=Marck+Script&family=Roboto:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet">
+    <style type='text/css'>
+        body,
+        html {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            background-image: url(https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v546batch3-mynt-34-badgewatercolor_1.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=89288ef4b47127f7f34a5998b50e4470);
+            color: black;
+            display: table;
+            font-family: Georgia, serif;
+            font-size: 18px;
+            text-align: center;
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            font-family: 'Roboto', sans-serif;
+        }
+
+        .container {
+            width: 100%;
+            height: 100%;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .logo {
+            position: absolute;
+            top: 20%;
+            left: 10%;
+            max-width: 10%;
+        }
+
+        .icon {
+            position: absolute;
+            top: 60%;
+            right: 10%;
+            max-width: 10%;
+        }
+
+        .title {
+            color: 870CA5;
+            font-size: 46px;
+            font-weight: bold;
+            font-family: 'Crimson Text', serif;
+        }
+
+        .subtitle {
+            font-size: 20px;
+        }
+
+        .course {
+            font-size: 52px;
+            margin: 16px;
+            font-family: 'Marck Script', cursive;
+        }
+
+        .assignment {
+            margin: 10px;
+            font-style: italic;
+            font-weight: 300;
+        }
+
+        .person {
+            border-bottom: 2px solid black;
+            font-size: 32px;
+            font-style: italic;
+            margin: 20px auto;
+            width: 400px;
+
+        }
+
+        .dotted {
+            border-bottom: 2px dotted #000;
+            text-decoration: none;
+        }
+
+        .reason {
+            margin: 20px;
+            max-width: 60%;
+            margin: auto;
+            line-height: 1.5;
+            font-style: italic;
+            font-weight: 300;
+        }
+
+        .foot-table {
+            margin: auto;
+            text-align: center;
+            margin-top: 8%;
+            width: 100%;
+        }
+
+        .foot-table td {
+            vertical-align: baseline;
+            font-size: 12px;
+        }
+
+        .sign-preson {
+	font-size: 14px;
+	margin: 5px;
+	color: #870CA5;
+	font-weight: bold;
+}
+    </style>
+</head>
+
+<body>
+    <div class="container">
+
+        <img class="logo" src="https://ipdedulk.com/admin/settings/logo/logo.png" alt="">
+        <img class="icon" src="https://ipdedulk.com/admin/settings/logo/logo.png" alt="">
+
+        <div class="title">
+            INSTITUTE FOR PROFESSIONAL
+            DEVELOPMENT
+        </div>
+
+        <div class="course">
+            Announcing Course
+        </div>
+
+        <div class="subtitle">
+            CERTIFICATE OF COMPLETION
+        </div>
+
+        <div class="assignment">
+            This is proudly presented to
+        </div>
+
+        <div class="person dotted">
+            Name
+        </div>
+
+        <div class="reason">
+            for participating and successfully completing
+            <br>
+            the Presentation & Announcing Course.
+            <br>
+            Awarded <span class="dotted">dotted text here</span>
+            <br>
+            The approved programme at IPD
+        </div>
+        <table class="foot-table">
+            <tr>
+                <td>
+                    <span class="dotted">dotted text here</span>
+
+                    <div class="sign-preson">
+                        Lal Sarath Kumara
+                    </div>
+                    Voice Over Artist
+                    <br>
+                    Television and Radio/Senior Lecturer
+                </td>
+                <td style="margin: 0 2%">
+                    <span class="dotted">dotted text here</span>
+
+                    <div class="sign-preson">
+                        K. A. Priyanka Perera
+                    </div>
+                    Consultant
+                    <br>
+                    Training and Skill Development
+                    <br>
+                    IPD Institute
+                </td>
+                <td>
+                    <span class="dotted">dotted text here</span>
+
+                    <div class="sign-preson">
+                        Chanaka Inoj
+                    </div>
+                    Senior Journalist / Lecturer &
+                    <br>
+                    News Anchor
+                </td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>
